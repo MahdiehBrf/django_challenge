2e084a692de807156875c01f81cad03da78421fc
Mahdieh Brf <mahdieh.brf95@gmail.com>
2022-01-29 02:03:49 +0330

    update readme

---
+++ blog/api.py
@@ -0,0 +1,36 @@
+# TODO: fill it
+
+
+posts = [
+    {
+        "title": "test",
+        "forms": {
+            "title": "درخواست نمایندگی",
+            "inputs": [
+                {
+                    "label": "شهر",
+                    "type": "choice",
+                    "choices": ["تهران", "مشهد", "تبریز"]
+                }
+            ],
+            "extra_data": {
+                "time_interval": 180
+            }
+        }
+    },
+    {
+        "title": "test",
+        "forms": {
+            "title": "سفارش کوله",
+            "inputs": [
+                {
+                    "label": "نام و نام خانوادگی",
+                    "type": "string",
+                }
+            ],
+            "extra_data": {
+                "time_interval": 60
+            }
+        }
+    }
+]
